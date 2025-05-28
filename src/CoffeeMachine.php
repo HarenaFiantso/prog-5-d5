07975e4d89b86a1b9eae69e41465c81045ae3150
Fiantso Harena <fiantsorav@gmail.com>
2025-05-28 09:31:31 +0300

    feat: can now pay with token method

--- src/CoffeeMachine.php
+++ src/CoffeeMachine.php
@@ -2,12 +2,23 @@
 
 namespace HarenaFiantso\Coffee;
 
+use HarenaFiantso\Coffee\Payment\CardPayment;
+use HarenaFiantso\Coffee\Payment\PaymentException;
+use HarenaFiantso\Coffee\Payment\TokenPayment;
+
 class CoffeeMachine
 {
+    /**
+     * @throws PaymentException
+     */
     public function makeCoffee(string $coffeeType, string $methodPayment): void
     {
         $coffee = CoffeeFactory::create($coffeeType);
 
-        echo $coffee->$methodPayment();
+        match ($methodPayment) {
+            'carte' => new CardPayment()->pay($coffee->getPrice()),
+            'jeton' => new TokenPayment()->pay($coffee->getPrice()),
+            default => throw new \InvalidArgumentException("Payment method not valid")
+        };
     }
 }
---
+++ src/Payment/TokenPayment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace HarenaFiantso\Coffee\Payment;
+
+class TokenPayment
+{
+    /**
+     * @throws PaymentException
+     */
+    public function pay(float $amount): void
+    {
+        $tokenValue = 1.5;
+
+        if ($amount > $tokenValue) {
+            throw new PaymentException("Jeton insufficient for this payment amount {$amount}€.");
+        }
+
+        echo "Payment {$amount}€ accepted\n";
+    }
+}
